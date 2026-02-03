18e89843d50851320eaef97d9e43350dcd07b135
memmedcanC <57996723+memmedcanC@users.noreply.github.com>
2026-02-03 20:21:52 +0400

    Commented

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println(s.next());
+        //salam eli bey dislemeye bilersinizmi
     }
 }
