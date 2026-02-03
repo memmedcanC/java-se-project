35c491fca9968c1bfc3ed3b4a488d22085ceaf9e
memmedcanC <57996723+memmedcanC@users.noreply.github.com>
2026-02-03 18:31:00 +0400

    New project apllied

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String a = "Java"+1+2+"Quiz"+(4+5);
-//salam
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.next());
     }
 }
