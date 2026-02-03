06bc5541a6be0dfce7747bddd51b307b1e71f4f9
memmedcanC <57996723+memmedcanC@users.noreply.github.com>
2026-02-03 19:12:28 +0400

    Comment added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println(s.next());
+        //salamlar
     }
 }
